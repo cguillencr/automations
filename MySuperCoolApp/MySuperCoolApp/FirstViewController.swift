5a76f88a06b894d2794f7e14c598d9593a104c62
cguillencr <cris.guillen0@gmail.com>
2020-04-20 16:18:56 -0600

    Demo App

---
+++ MySuperCoolApp/MySuperCoolApp/FirstViewController.swift
@@ -0,0 +1,28 @@
+//
+//  FirstViewController.swift
+//  MySuperCoolApp
+//
+//  Created by TesterUser on 3/26/20.
+//  Copyright © 2020 Testingsoft. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController,UITextFieldDelegate {
+    
+  //  @IBOutlet weak var myTextField : UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //02 set delegate to textfield
+    //   self.myTextField.delegate = self
+    }
+
+   /*func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+       self.view.endEditing(true)
+       return false;
+   }*/
+
+}
+
+	
---
+++ MySuperCoolApp/MySuperCoolApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MySuperCoolApp
+//
+//  Created by TesterUser on 3/26/20.
+//  Copyright © 2020 Testingsoft. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
